c421b7f3ef3184a07a0fc0a42d3600d690fbf548
Mark Ian Pamintuan <ianmayfire@gmail.com>
2017-06-12 22:37:45 +0800

    First commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Mark Ian Pamintuan">
+    <meta name="description" content="Developer Portfolio">
+    <meta name="keywords" content="Web Development, Desktop Development, Portfolio, Freelancer, Programmer, HTML, JavaScript, CSS, PHP, Wordpress, Visual Basic.Net, MySQL">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>My Portfolio</title>
+</head>
+<body>
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#mainNavbar" role="navigation">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">
+                    Mark Ian Pamintuan
+                </a>
+            </div>
+            <div class="collapse navbar-collapse" id="mainNavbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a id="home" href="#">Home</a></li>
+                    <li><a href="#works" >About Me</a></li>
+                    <li><a href="#works" >Education</a></li>
+                    <li><a href="#works" >Works</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <section>
+        <div id="lead">
+            
+            <div id="lead-content">
+                <h1>MARK IAN PAMINTUAN</h1>
+                <h2>Web & Desktop Developer</h2>
+                <a href="#" class="btn btn-default" id="resume">Download Resume</a>
+            </div>
+            <div id="lead-overlay"></div>
+        </div>
+    </section>
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="js/script.js"></script>
+</body>
+</html>
