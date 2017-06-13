535189cc2c76570646f9b07958df0d6dc797d3d0
Mark Ian Pamintuan <ianmayfire@gmail.com>
2017-06-13 21:46:50 +0800

    Added education, projects and skills section

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <meta name="keywords" content="Web Development, Desktop Development, Portfolio, Freelancer, Programmer, HTML, JavaScript, CSS, PHP, Wordpress, Visual Basic.Net, MySQL">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
     <title>My Portfolio</title>
 </head>
@@ -27,19 +28,18 @@
             </div>
             <div class="collapse navbar-collapse" id="mainNavbar">
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a id="home" href="#">Home</a></li>
-                    <li><a href="#works" >About Me</a></li>
-                    <li><a href="#works" >Education</a></li>
-                    <li><a href="#works" >Works</a></li>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#education">Education</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#skills">Skills</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </ul>
             </div>
         </div>
     </nav>
 
-    <section>
+    <section id="home">
         <div id="lead">
-            
             <div id="lead-content">
                 <h1>MARK IAN PAMINTUAN</h1>
                 <h2>Web & Desktop Developer</h2>
@@ -49,6 +49,91 @@
         </div>
     </section>
 
+    <section>
+        <div id="education">
+            <header>
+                <h2 class="text-center">Education</h2>
+            </header>
+            <div class="education-block">
+                <h3>Don Honorio Ventura Technological State University</h3>
+                <span class="education-date">June 2012 - March 2016</span>
+                <h4>Bachelor of Science in Information Technology</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in iaculis ex. Etiam volutpat laoreet urna. Morbi ut tortor nec nulla commodo malesuada sit amet vel lacus. Fusce eget efficitur libero. Morbi dapibus porta quam laoreet placerat.</p>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div id="projects">
+            <header>
+                <h2 class="text-center">Projects</h2>
+            </header>
+            <div class="projects-block">
+                <div class="row">
+                    <div class="col-sm-6 col-md-4">
+                        <div class="thumbnail">
+                        <img src="img/main-image.jpeg" alt="image">
+                        <div class="caption">
+                            <h3>Project Name</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in iaculis ex. Etiam volutpat laoreet urna. Morbi ut tortor nec nulla commodo malesuada sit amet vel lacus. Fusce eget efficitur libero. Morbi dapibus porta quam laoreet placerat.</p>
+                            <p><a href="#" class="btn btn-primary" role="button">Details</a></p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4">
+                        <div class="thumbnail">
+                        <img src="img/main-image.jpeg" alt="image">
+                        <div class="caption">
+                            <h3>Project Name</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in iaculis ex. Etiam volutpat laoreet urna. Morbi ut tortor nec nulla commodo malesuada sit amet vel lacus. Fusce eget efficitur libero. Morbi dapibus porta quam laoreet placerat.</p>
+                            <p><a href="#" class="btn btn-primary" role="button">Details</a></p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4">
+                        <div class="thumbnail">
+                        <img src="img/main-image.jpeg" alt="image">
+                        <div class="caption">
+                            <h3>Project Name</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in iaculis ex. Etiam volutpat laoreet urna. Morbi ut tortor nec nulla commodo malesuada sit amet vel lacus. Fusce eget efficitur libero. Morbi dapibus porta quam laoreet placerat.</p>
+                            <p><a href="#" class="btn btn-primary" role="button">Details</a></p>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </section>
+
+    <section>
+        <div id="skills">
+            <header>
+                <h2 class="text-center">Skills</h2>
+            </header>
+            <div class="skills-block">
+                <ul id="skills-list">
+                    <li><h3><span class="label label-default">HTML</span></h3></li>
+                    <li><h3><span class="label label-default">CSS</span></h3></li>
+                    <li><h3><span class="label label-default">JavaScript</span></h3></li>
+                    <li><h3><span class="label label-default">jQuery</span></h3></li>
+                    <li><h3><span class="label label-default">Bootstrap</span></h3></li>
+                    <li><h3><span class="label label-default">PHP</span></h3></li>
+                    <li><h3><span class="label label-default">Codeigniter</span></h3></li>
+                    <li><h3><span class="label label-default">Wordpress</span></h3></li>
+                    <li><h3><span class="label label-default">Visual Basic.Net</span></h3></li>
+                    <li><h3><span class="label label-default">C++</span></h3></li>
+                    <li><h3><span class="label label-default">Java</span></h3></li>
+                    <li><h3><span class="label label-default">MySQL</span></h3></li>
+                    <li><h3><span class="label label-default">Git</span></h3></li>
+                    <li><h3><span class="label label-default">Microsoft Office Applications</span></h3></li>
+                    <li><h3><span class="label label-default">Adobe Flash</span></h3></li>
+                    <li><h3><span class="label label-default">Adobe Flex Builder</span></h3></li>
+                    <li><h3><span class="label label-default">Adobe Photoshop</span></h3></li>
+                </ul>
+            </div>
+        </div>
+    </section>
 
     <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
