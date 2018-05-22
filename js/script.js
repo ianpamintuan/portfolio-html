4442331b68001806240f883507f18eb0ba32a9c1
Mark Ian Pamintuan <ianmayfire@gmail.com>
2018-05-22 09:48:43 +0800

    Added scrollspy smooth scrolling on navigation

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    
+    $('body').scrollspy({target: ".navbar", offset: 50});   
+  
+    $("#mainNavbar a").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+    
+            var hash = this.hash;
+    
+            $('html, body').animate({
+            scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash;
+            });
+        }
+
+    });
+
+});
